4a1b332a5242d5ba647a17b7128a00b1fd968702
Jon Bennett <jonbennett@jbdatasolutions.com>
2024-04-06 14:34:50 -0500

    RCURS-22: Limit concurrency to 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,26 @@
-use std::env;
-use std::sync::Arc;
-use std::time::Duration;
-use db::{connect, DB};
+use crate::config::Config;
 use anyhow::Result;
-use sqlx;
+use db::{connect, DB};
+use dotenv::dotenv;
+use futures::{stream, StreamExt};
+use log::{info, warn};
+use serde::Serialize;
+use skillratings::weng_lin::weng_lin_multi_team;
 use skillratings::{
     weng_lin::{WengLinConfig, WengLinRating},
     MultiTeamOutcome,
 };
-use skillratings::weng_lin::weng_lin_multi_team;
-use log::{info, warn};
-use dotenv::dotenv;
-use task_queue::queue::{Job, Queue, Message, MessageScope};
-use futures::{stream, StreamExt};
-use task_queue::job::{DbQueue};
-use serde::Serialize;
-use crate::config::Config;
-
+use sqlx;
+use std::env;
+use std::sync::Arc;
+use std::time::Duration;
+use task_queue::job::DbQueue;
+use task_queue::queue::{Job, Message, MessageScope, Queue};
 
 pub mod config;
 pub mod db;
 
-const CONCURRENCY: usize = 10;
+const CONCURRENCY: usize = 1;
 
 pub async fn run() -> Result<()> {
     dotenv().ok();
@@ -44,7 +43,7 @@ pub async fn run() -> Result<()> {
 
         let _ = queue.push(job, MessageScope::GenerateRatings, None).await?;
         queue.heartbeat(MessageScope::GenerateRatings).await?;
-    }else if config.run{
+    } else if config.run {
         queue.heartbeat(MessageScope::GenerateRatings).await?;
     }
 
@@ -53,7 +52,10 @@ pub async fn run() -> Result<()> {
 
 async fn run_worker(queue: Arc<dyn Queue>) {
     loop {
-        let jobs = match queue.pull(MessageScope::GenerateRatings, CONCURRENCY as i32).await {
+        let jobs = match queue
+            .pull(MessageScope::GenerateRatings, CONCURRENCY as i32)
+            .await
+        {
             Ok(jobs) => jobs,
             Err(e) => {
                 warn!("Error pulling jobs: {}", e);
@@ -75,7 +77,7 @@ async fn run_worker(queue: Arc<dyn Queue>) {
                     Ok(_) => {
                         info!("Job {} completed", job_id);
                         queue.delete_job(job_id).await
-                    },
+                    }
                     Err(e) => {
                         warn!("Job {} failed: {}", job_id, e);
                         queue.fail_job(job_id).await
@@ -108,10 +110,10 @@ async fn process_job(job: Job) -> Result<()> {
     Ok(())
 }
 
-#[derive(Debug, Clone, Serialize,sqlx::FromRow)]
+#[derive(Debug, Clone, Serialize, sqlx::FromRow)]
 struct EventRaceInfoResponse {
     class: String,
-    race: String
+    race: String,
 }
 
 async fn get_event_races(event: i64, pool: &DB) -> Result<Vec<EventRaceInfoResponse>> {
@@ -120,34 +122,34 @@ async fn get_event_races(event: i64, pool: &DB) -> Result<Vec<EventRaceInfoRespo
         FROM event_overall_ranking WHERE event_id = $1
         ";
 
-    let event_results = sqlx::query_as::<_,EventRaceInfoResponse>(query)
+    let event_results = sqlx::query_as::<_, EventRaceInfoResponse>(query)
         .bind(event)
         .fetch_all(pool)
         .await?;
 
     Ok(event_results)
-    }
+}
 
-#[derive(Debug, Clone, Serialize,sqlx::FromRow)]
+#[derive(Debug, Clone, Serialize, sqlx::FromRow)]
 struct EventResultResponse {
     driver_id: i32,
     event_id: i64,
     class: String,
     race: String,
     rating: Option<i64>,
-    position: i32
+    position: i32,
 }
 
 #[derive(Debug, Clone)]
 struct EventResult {
     event_id: i64,
-    results: Vec<EventResultResponse>
+    results: Vec<EventResultResponse>,
 }
 
 async fn get_event(event: i64, pool: &DB) -> Result<Vec<EventResult>> {
     let races = get_event_races(event, pool).await?;
     let mut event_results: Vec<EventResult> = Vec::new();
-    for race in races  {
+    for race in races {
         let query = "
         SELECT
             driver.id as driver_id,
@@ -166,38 +168,59 @@ async fn get_event(event: i64, pool: &DB) -> Result<Vec<EventResult>> {
         let class = race.class.clone();
         let this_race = race.race.clone();
 
-        let race_result = sqlx::query_as::<_,EventResultResponse>(query)
+        let race_result = sqlx::query_as::<_, EventResultResponse>(query)
             .bind(event)
             .bind(class)
             .bind(this_race)
             .fetch_all(pool)
             .await?;
 
-
-        event_results.push(EventResult{event_id: event, results: race_result});
+        event_results.push(EventResult {
+            event_id: event,
+            results: race_result,
+        });
     }
     Ok(event_results)
 }
 
-async  fn recalculate_ratings(event_results: Vec<EventResult>, pool: &DB) -> Result<()> {
-    info!("Recalculating ratings for event {}", event_results[0].event_id);
-    const DEFAULT_UNCERTAINTY: i64 = 25/3;
+async fn recalculate_ratings(event_results: Vec<EventResult>, pool: &DB) -> Result<()> {
+    if event_results.len() == 0 {
+        return Ok(());
+    }
+    info!(
+        "Recalculating ratings for event {}",
+        event_results[0].event_id
+    );
+    const DEFAULT_UNCERTAINTY: i64 = 25 / 3;
     for event_result in event_results {
         let mut teams_and_ranks: Vec<(Vec<WengLinRating>, MultiTeamOutcome)> = vec![];
         for result in &event_result.results {
             let mut player_rating: Vec<WengLinRating> = vec![];
-            player_rating.push(WengLinRating{rating: result.rating.unwrap_or(1000) as f64, uncertainty: DEFAULT_UNCERTAINTY as f64});
+            player_rating.push(WengLinRating {
+                rating: result.rating.unwrap_or(1000) as f64,
+                uncertainty: DEFAULT_UNCERTAINTY as f64,
+            });
             let outcome = MultiTeamOutcome::new(result.position as usize);
             teams_and_ranks.push((player_rating, outcome));
         }
-        let new_ratings = weng_lin_multi_team(&teams_and_ranks.iter().map(|(rating, outcome)| (rating.as_slice(), *outcome)).collect::<Vec<_>>(), &WengLinConfig::default());
+        let new_ratings = weng_lin_multi_team(
+            &teams_and_ranks
+                .iter()
+                .map(|(rating, outcome)| (rating.as_slice(), *outcome))
+                .collect::<Vec<_>>(),
+            &WengLinConfig::default(),
+        );
         update_ratings(&event_result.clone(), &new_ratings, &pool).await?;
     }
     Ok(())
 }
 
-async fn update_ratings(event_result: &EventResult, new_ratings: &Vec<Vec<WengLinRating>>, pool: &DB) -> Result<()> {
-    for (i,rating) in new_ratings.iter().enumerate() {
+async fn update_ratings(
+    event_result: &EventResult,
+    new_ratings: &Vec<Vec<WengLinRating>>,
+    pool: &DB,
+) -> Result<()> {
+    for (i, rating) in new_ratings.iter().enumerate() {
         let driver_id = event_result.results[i].driver_id as i32;
         let new_rating = rating[0].rating as i64;
         let query = "
@@ -213,5 +236,3 @@ async fn update_ratings(event_result: &EventResult, new_ratings: &Vec<Vec<WengLi
     }
     Ok(())
 }
-
-
